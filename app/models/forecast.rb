e17b1f45017795b370a54ae57b0f811cc4538ba8
benjamintillett <benjamin.tillett@gmail.com>
2014-10-15 15:14:20 +0100

    passing specs for forecast

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -8,18 +8,11 @@ class Forecast
 	
 	def get_forecast
 		forecast_string = open("https://api.forecast.io/forecast/967ecda5e55eea73c15e3a4ce315e508/#{@location}").read 
-		forecast = JSON.parse(forecast_string) rescue  " "
-		puts "-----------"
-		puts forecast.class
-		puts forecast_string.class
-		#@summary = forecast['minutely']['summary']
+		forecast = JSON.parse(forecast_string)  rescue  "There was an error obtaining the forecast"
 	end
 
-	def summary
-		response_forecast = get_forecast
-		puts "XXXX"*20
-		puts response_forecast
-		@forecast = response_forecast['minutely']['summary']
+	def summary	
+		@forecast = get_forecast['minutely']['summary']
 	end
 end
 
--- spec/models/forecast_io_spec.rb
+++ spec/models/forecast_io_spec.rb
@@ -2,30 +2,26 @@ require_relative "../helper_files"
 
 include TestFiles
 
-#puts FORECAST_IO_JSON_RESPONE
 
 
 describe Forecast do 
 
 	let(:weather) { Forecast.new("51.5231,-0.0871") }
+	let(:stub_weather) { stub_request(:get, "https://api.forecast.io/forecast/967ecda5e55eea73c15e3a4ce315e508/51.5231,-0.0871")
+			.to_return(body: FORECAST_IO_JSON_RESPONE, status: 200) }
 
 	
 
 	context "Getting the forecast" do 
 		it "can get the forecast for London in JSON" do
-			stub_request(:get, "https://api.forecast.io/forecast/967ecda5e55eea73c15e3a4ce315e508/51.5231,-0.0871")
+			stub_weather
 			weather.get_forecast
 			expect(a_request(:get, "https://api.forecast.io/forecast/967ecda5e55eea73c15e3a4ce315e508/51.5231,-0.0871")).to have_been_made
 		end
 
 		it "returns a weather summary" do
-				stub_request(:get, "https://api.forecast.io/forecast/967ecda5e55eea73c15e3a4ce315e508/51.5231,-0.0871").
-				to_return(body: FORECAST_IO_JSON_RESPONE, status: 200)
-				puts FORECAST_IO_JSON_RESPONE['minutely']['summary']
-
-
-
-				expect(weather.summary).to eq ("Mostly cloudy for the hour")
+			stub_weather
+			expect(weather.summary).to eq ("Partly cloudy for the hour.")
 		end
 	end
 end
