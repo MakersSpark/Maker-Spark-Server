064946c5cd566a460a48f3d1708c1f147e38f281
kikrahau <vincent.koch@hotmail.com>
2014-10-16 17:49:22 +0100

    tested guthubdata class

--- spec/models/github_data_spec.rb
+++ spec/models/github_data_spec.rb
@@ -2,20 +2,41 @@ describe GithubData do
 
 	let(:github) { GithubData.new('byverdu') }
 	let(:stats)  { double :github_stats }
+	let(:streak) { double :streak }
+	let(:longest_streak) { double :longest_streak }
+	let(:max) { double :max }
+	let(:data) { double :data, streak: streak, longest_streak: longest_streak, max: max }
 	
-	context "using the GithubStats gem" do
+	context "requiring contributions from the GithubStats gem" do
 
 		before do 
-			
 			GithubStats.stub(:new).and_return(stats)
 		end
 
 		it 'can get the current streak of the user' do 
-			streak = double :streak
-			data = double :data, streak: streak
 			allow(stats).to receive(:data).and_return(data)
 			expect(streak).to receive(:count)
 			github.current_streak
+		end
+
+		it 'can get the longest_streak of the user' do 
+			allow(stats).to receive(:data).and_return(data)
+			expect(longest_streak).to receive(:count)
+			github.longest_streak
+		end
+
+		it 'can get the score of today' do 
+			allow(stats).to receive(:data).and_return(data)
+			expect(data).to receive(:today)
+			github.score_today
+		end
+
+		it 'can get highscore' do 
+			allow(stats).to receive(:data).and_return(data)
+			allow(data).to receive(:max).and_return(max)
+			expect(max).to receive(:score)
+			expect(max).to receive(:date)
+			github.highscore
 		end
 	end
 end
