8a9e8918e11e5abecb8a9a63034359a6d4ce485a
byverdu <byverdu@gmail.com>
2014-10-16 10:08:52 +0100

    user can sign up test failing

--- spec/features/user_signup_feature_spec.rb
+++ spec/features/user_signup_feature_spec.rb
@@ -13,7 +13,28 @@ feature "User sign up" do
 			expect(page).to have_selector("input[name=password]")
 			expect(page).to have_selector("input[name=password_confirmation]")
 			expect(page).to have_button("Sign")
-    	end
+    end
+
+    scenario "The user can sign up" do
+
+    	expect{ sign_up }.to change(User, :count).by(1)
+			expect(page).to have_content('Welcome byverdu@test.com')
+			expect(User.first.email).to eq('byverdu@test.com')
+
+    end
 	end
 
+end
+
+def sign_up(email                 =  'byverdu@test.com',
+	          password              = 's3cr3t',
+	          password_confirmation = 's3cr3t')
+
+	visit '/sign_up'
+
+	fill_in 'email', with: email
+	fill_in 'password', with: password
+	fill_in 'password_confirmation', with: password_confirmation
+
+	click_button 'Sign up'
 end
