34ae91ba4d70f3d7f44744a1f8eec8d01c1deb67
Henry Stanley <henry@henrystanley.com>
2014-10-17 10:53:28 +0100

    add check printer response method and feature test for unsucessfull print

--- app/models/printer.rb
+++ app/models/printer.rb
@@ -9,11 +9,21 @@ class Printer
 
 	def print_line(line)
 		format, text = line[0], line[1]
-		p Net::HTTP.post_form(print_uri, access_token: @spark_api_token , args: "#{format}=#{text}/")
-		"Successfully sent to the printer!"
+		response = Net::HTTP.post_form(print_uri, access_token: @spark_api_token , args: "#{format}=#{text}/") 
+		check_printer_response(response)
 	end
 
 	def print(message)
 		message.lines.each{|line| print_line(line)}
 	end
+
+	def check_printer_response(response)
+		if JSON.parse(response.body)["return_value"] == 1 
+			"Successfully sent to the printer!"
+		else
+			"Sorry, something went wrong. Is the printer online?"
+		end
+	end
+
+
 end
