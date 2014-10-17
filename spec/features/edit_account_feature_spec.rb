a9fee2a899529f103e30b17c84479cae2f161e69
byverdu <byverdu@gmail.com>
2014-10-17 15:51:16 +0100

    added edit_user.rb and edit_account_feature_spec.rb

---
+++ spec/features/edit_account_feature_spec.rb
@@ -0,0 +1,31 @@
+feature "Editing a user account" do
+
+	before do
+		stub_request(:any, "https://github.com/users/byverdu/contributions")
+		stub_request(:any, "https://github.com/users/henryaj/contributions")
+		sign_up
+		sign_in
+		click_link('Edit account') 	
+	end
+
+	scenario "should be able to see the form" do
+
+		expect(current_path).to eq('/edit_user')
+		expect(page).to have_selector("form.edit_user")
+		expect(page).to have_content('Editing the account')
+	end
+
+	scenario "Email and github user will be displayed" do
+
+		expect(current_path).to eq('/edit_user')
+		expect(page).to have_xpath("//input[@value='byverdu@test.com']")
+		expect(page).to have_xpath("//input[@value='byverdu']")
+	end
+
+	scenario "can change all his personal details" do
+		editing_user
+		
+		expect(page).to have_content('Your details has been successfully updated')
+		expect(current_path).to eq('/')
+	end
+end
