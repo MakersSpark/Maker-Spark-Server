12f03222d31749b3e2df8570d9c158b76b966f8e
Henry Stanley <henry@henrystanley.com>
2014-10-17 16:32:16 +0100

    refactored Calendar methods (thanks @benjamintillett!)

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -10,16 +10,33 @@ class Calendar
   end
 
   def get_events
-    puts "-------------------"
-    # data.events.each {|e| puts e.rrule.frequency }
-    data.events.each {|e| puts e.rrule.first.frequency }
-    puts "-------------------"
-
-    @todays_events += data.events.select{ |event| event.rrule == [] && event.dtstart.today? } # gets events that do not repeat and are today
-    @todays_events += data.events.select{ |event| event.rrule.first.frequency == "DAILY"  }
-    @todays_events += data.events.select{ |event| event.rrule.first.frequency == "WEEKLY" &&  event.dtstart.strftime('%A') == DateTime.now.strftime('%A') }
-    @todays_events += data.events.select{ |event| event.rrule.first.frequency == "MONTHLY" && event.dtstart.strftime('%d') == DateTime.now.strftime('%d') }
+    get_todays_non_recurring_events
+    reject_non_recurring_events
+    get_daily_events
+    get_weekly_events
+    get_monthly_events
   end
+
+  def get_todays_non_recurring_events
+    @todays_events += data.events.select{ |event| event.rrule == [] && event.dtstart.today? } # gets events that do not repeat and are today
+  end
+
+  def reject_non_recurring_events
+    @sanitised_events = data.events.reject{ |event| event.rrule == [] }
+  end
+
+  def get_daily_events
+    @todays_events += @sanitised_events.select{ |event| event.rrule.first.frequency == "DAILY"  }
+  end
+  def get_weekly_events
+    @todays_events += @sanitised_events.select{ |event| event.rrule.first.frequency == "WEEKLY" && event.dtstart.strftime('%A') == DateTime.now.strftime('%A') }
+  end
+
+  def get_monthly_events
+    @todays_events += @sanitised_events.select{ |event| event.rrule.first.frequency == "MONTHLY" && event.dtstart.strftime('%d') == DateTime.now.strftime('%d') }
+  end
+
+
 
 
 end
