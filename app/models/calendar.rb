f578d76cbefac145daac54d2f5987af9715df67f
Henry Stanley <henry@henrystanley.com>
2014-10-17 15:26:08 +0100

    updated Calendar model to find recurring events :triumph:

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,3 +1,25 @@
 class Calendar
 
+  attr_accessor :uri, :todays_events
+  attr_reader :data
+
+  def initialize(uri)
+    @uri = uri
+    @data = Icalendar.parse(open(@uri)).first
+    @todays_events = []
+  end
+
+  def get_events
+    puts "-------------------"
+    # data.events.each {|e| puts e.rrule.frequency }
+    data.events.each {|e| puts e.rrule.first.frequency }
+    puts "-------------------"
+
+    @todays_events += data.events.select{ |event| event.rrule == [] && event.dtstart.today? } # gets events that do not repeat and are today
+    @todays_events += data.events.select{ |event| event.rrule.first.frequency == "DAILY"  }
+    @todays_events += data.events.select{ |event| event.rrule.first.frequency == "WEEKLY" &&  event.dtstart.strftime('%A') == DateTime.now.strftime('%A') }
+    @todays_events += data.events.select{ |event| event.rrule.first.frequency == "MONTHLY" && event.dtstart.strftime('%d') == DateTime.now.strftime('%d') }
+  end
+
+
 end
