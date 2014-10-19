33e78901ff0b8a3f3746428feea50dd3d6789230
kikrahau <vincent.koch@hotmail.com>
2014-10-19 14:16:40 +0100

    added Rakefile for upgrading/migrating data

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -3,5 +3,3 @@ env = ENV["RACK_ENV"] || "development"
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/spark_print_#{env}")
 
 DataMapper.finalize
-
-DataMapper.auto_migrate!
