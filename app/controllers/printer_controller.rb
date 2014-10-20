8538bf177edb92fd4d83c8d10aa3f10da9e622ce
kikrahau <vincent.koch@hotmail.com>
2014-10-20 09:11:53 +0100

    moved printer to sparkPrint controller

--- app/controllers/printer_controller.rb
+++ app/controllers/printer_controller.rb
@@ -1,14 +1,7 @@
 require_relative "../server.rb"
 
 class PrinterController < SparkPrint
-    post "/" do 
-      card_info = JsonHandler.get_user_info(params[:data]) 
-      user = User.first(rfid_code: card_info["data"])
-      event = EventHandler.new(card_info, user)
-      event.build_message           
-      event.print_message(Printer.new)
-      "sorry ben is stupid"
-    end
+
 
     post "/print" do 
       printer = Printer.new
