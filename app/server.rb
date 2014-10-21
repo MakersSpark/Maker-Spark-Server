b496851a6e18eb7e14e641833d46ca519ce514dd
kikrahau <vincent.koch@hotmail.com>
2014-10-21 15:33:11 +0100

    merge conflict

--- app/server.rb
+++ app/server.rb
@@ -75,9 +75,6 @@ class SparkPrint < Sinatra::Base
     event.build_message           
     event.print_message(printer)
     event.delete_user_messages(printer.response)
-    puts "-------------------------------"
-    puts printer.response
-    puts "-------------------------------"
     "sorry ben is stupid"
   end
 
