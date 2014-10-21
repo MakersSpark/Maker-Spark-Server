c315bc388a5ce4f84a1b9ec80358583db50c4f1f
kikrahau <vincent.koch@hotmail.com>
2014-10-21 10:56:19 +0100

    add json processor

---
+++ spec/models/json_processor_spec.rb
@@ -0,0 +1,10 @@
+# describe JsonProcessor do 
+
+# 	let(:processor) { JsonProcessor.new }
+# 	let(:user_options) {   }
+	
+# 	it "can create a JSON with the users options" do 
+# 		expect(processor.user_options() ).to eq {}
+# 	end
+
+# end
